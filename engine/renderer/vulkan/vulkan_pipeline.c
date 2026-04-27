468f72087350f7efd51f03fbe979522d4829af6d
Amalg016 <amalgjoseph1@gmail.com>
2026-04-27 07:29:09 +0530

    fix: use clockwise front face to match Vulkan's Y-down framebuffer
    Vulkan's framebuffer coordinates have Y pointing downward, making our
    quad vertices wind clockwise in screen space.  With the pipeline set to
    COUNTER_CLOCKWISE, both triangles were culled as back-facing, resulting
    in an invisible quad.

--- engine/renderer/vulkan/vulkan_pipeline.c
+++ engine/renderer/vulkan/vulkan_pipeline.c
@@ -245,7 +245,7 @@ bool vulkan_pipeline_create(VulkanContext *ctx,
         .polygonMode = VK_POLYGON_MODE_FILL,
         .lineWidth   = 1.0f,
         .cullMode    = VK_CULL_MODE_BACK_BIT,
-        .frontFace   = VK_FRONT_FACE_COUNTER_CLOCKWISE,
+        .frontFace   = VK_FRONT_FACE_CLOCKWISE,
     };
 
     VkPipelineMultisampleStateCreateInfo multisample = {
